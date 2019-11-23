73ef13e881ada3878a9c7bf12d1da77683df4987
Tsz Lam Yau <yaurfu@gmail.com>
2019-11-23 21:12:40 +0800

    Move menus to func

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -2,9 +2,7 @@ import Vue from 'vue';
 import ContextMenu from './ContextMenu.vue';
 import $ from 'jquery';
 
-const menus = [];
-
-export default function (binding, configs, zIndex = 1000) {
+export default function (binding, configs, menus = [], zIndex = 1000) {
   const vContextMenu = Vue.extend(ContextMenu);
   const vInstance = new vContextMenu({
     parent: binding,
